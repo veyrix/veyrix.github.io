6799b533259a41c1e97720473812ff2ca5715586
Felix Avelar <felixavco@gmail.com>
2018-11-15 01:51:30 -0600

    updated Javascript

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
 const targets =  document.querySelectorAll('.target')
 const scoreDisplay = document.querySelector('#score')
 const container = document.querySelector('.container')
-let score = 0
+let score = 0;
 
+/*Changes and updates the page title with the current score */
+document.title += ` - Your Score ${score}`
+
+/*Adds the current score to the score display*/
+scoreDisplay.innerHTML = score
+
+/*Mouse events to animate the cursor */
 container.addEventListener('mousedown', changeCursor)
 container.addEventListener('mouseup', restoreCursor)
 
-
-
 function changeCursor() {
   this.style.cursor = "url('../img/Hammer.90deg.cur'), auto"
-  console.log("mousedown")
 } 
 
 function restoreCursor() {
   this.style.cursor = "url('../img/Hammer.cur'), auto"
-  console.log("mouseup")
 } 
+
+
 
